0bdc35215aedcef0679d7c43871a6225b9489a2e
Goran Mržljak <goran.mrzljak@gmail.com>
2026-04-21 23:01:50 +0200

    add icons

--- src/app/intro-page.tsx
+++ src/app/intro-page.tsx
@@ -1,12 +1,9 @@
+import { Icon } from '@iconify/react';
 import type { CSSProperties, FC } from 'react';
 
 import viteLogo from '/vite.svg';
 
 import reactLogo from '../assets/react.svg';
-
-const imageContainerStyle: CSSProperties = {
-  height: '4rem',
-};
 
 const imageStyle: CSSProperties = {
   display: 'flex',
@@ -19,9 +16,22 @@ export const IntroPage: FC = () => {
     <div>
       <h1>template-react</h1>
       <p className='bg-orange-200 text-2xl text-blue-500'>App is running.</p>
-      <div style={imageContainerStyle}>
+      <div>
         <img alt='Vite logo' src={viteLogo} style={imageStyle} />
         <img alt='React logo' src={reactLogo} style={imageStyle} />
+      </div>
+      <div>
+        <Icon icon='cif:hr' width='500' height='300' />
+        <Icon icon='mdi:linkedin' width='48' height='48' color='#0a66c2' />
+        <Icon icon='mdi:github' width='48' height='48' color='#181717' />
+        <Icon icon='mdi:stackoverflow' width='48' height='48' color='#f48024' />
+        <Icon
+          icon='mdi:file-pdf-box'
+          width='48'
+          height='48'
+          color='#ef4444' // classic PDF red
+        />
+        <Icon icon='material-symbols:download' width='48' height='48' />
       </div>
     </div>
   );
