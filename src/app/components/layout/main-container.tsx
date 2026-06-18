68dbd25d12c2f3829d4315c54aeb93c01dc2729d
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-18 20:31:27 +0200

    fix props type

--- src/app/components/layout/main-container.tsx
+++ src/app/components/layout/main-container.tsx
@@ -1,10 +1,10 @@
 import type { FC, ReactNode } from 'react';
 
-export interface Props {
+export interface MainContainerProps {
   readonly children: ReactNode;
 }
 
-export const MainContainer: FC<Props> = ({ children }) => {
+export const MainContainer: FC<MainContainerProps> = ({ children }) => {
   return (
     <div className='min-h-screen text-slate-700 dark:text-slate-300'>
       <div className='mx-auto w-full px-4 sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl'>
