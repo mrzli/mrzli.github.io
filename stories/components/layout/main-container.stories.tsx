efcf4b93f6084ffdf0421b8d866fc8349998530f
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-18 22:29:17 +0200

    add main container story

---
+++ stories/components/layout/main-container.stories.tsx
@@ -0,0 +1,23 @@
+import { MainContainer } from '@components';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const meta = {
+  component: MainContainer,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof MainContainer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <div className='h-screen bg-slate-300 dark:bg-slate-800'>
+        Main container content.
+      </div>
+    ),
+  },
+};
