a2f9eb5dec5283130e40d983d8c44da63469ae16
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-20 16:57:31 +0200

    update test-page

--- src/app/pages/test-page.tsx
+++ src/app/pages/test-page.tsx
@@ -2,9 +2,8 @@ import {
   Card,
   ExternalLink,
   PageContainer,
-  Tag,
+  TagList,
   Typography,
-  UnorderedList,
 } from '@components';
 import type { ReactNode } from 'react';
 
@@ -12,128 +11,110 @@ import type { LinkItem } from '../types';
 
 export function TestPage(): ReactNode {
   return (
-    <PageContainer title='Background'>
-      <div className='grid gap-4 lg:grid-cols-6'>
-        <Card className='md:col-span-3 lg:col-span-3' padding='large'>
-          <div className='flex flex-col gap-6'>
-            <div className='flex flex-col gap-2'>
+    <PageContainer title='Projects'>
+      <div className='flex flex-col gap-6'>
+        <Typography variant='body'>Projects I did in my free time.</Typography>
+        <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+          <Card padding='large'>
+            <div className='flex flex-col gap-4'>
               <Typography variant='card-title-accent'>
-                Formal Education
-              </Typography>
-              <Typography variant='heading-2'>
-                Faculty of Electrical Engineering and Computing (FER),
-                University of Zagreb
+                Personal Website
               </Typography>
               <Typography variant='body'>
-                Graduate Engineer of Computing
+                You are currently viewing it. It is just a simple static website
+                displaying information relevant to my professional life.
               </Typography>
-            </div>
+              <TagList tags={PROJECT_PERSONAL_WEBSITE_TAGS} />
 
-            <UnorderedList items={EDUCATION_HIGHLIGHTS} />
+              <Card variant='inner' padding='small'>
+                <div className='flex flex-col gap-2'>
+                  <Typography variant='heading-4'>My role</Typography>
+                  <Typography variant='body'>
+                    I did all the development. The site is static and very
+                    simple. All of the content of the pages is simply hardcoded
+                    into the components.
+                  </Typography>
+                </div>
+              </Card>
 
-            <Card variant='highlight' padding='medium'>
-              <div className='flex flex-col gap-3'>
-                <Typography variant='heading-3'>Related links</Typography>
+              <Card variant='highlight' padding='small'>
                 <ul className='flex flex-col gap-2'>
-                  {THESIS_LINKS.map((link) => {
+                  {PROJECT_PERSONAL_WEBSITE_LINKS.map((link) => {
                     return (
-                      <li key={link.href}>
+                      <div key={link.href} className='flex'>
                         <ExternalLink
                           variant='highlight'
                           href={link.href}
                           label={link.label}
                         />
-                      </li>
+                      </div>
                     );
                   })}
                 </ul>
-              </div>
-            </Card>
-          </div>
-        </Card>
-
-        <Card className='md:col-span-3 lg:col-span-3' padding='large'>
-          <div className='flex flex-col gap-5'>
-            <div className='flex flex-col gap-2'>
-              <Typography variant='card-title-accent'>
-                Additional Learning
-              </Typography>
-              <Typography variant='heading-2'>
-                Structured learning outside formal education
-              </Typography>
+              </Card>
             </div>
+          </Card>
 
-            <Typography variant='body'>
+          <Card className='md:col-span-3 lg:col-span-3' padding='large'>
+            <div className='flex flex-col gap-5'>
               <div className='flex flex-col gap-2'>
-                {ADDITIONAL_LEARNING_PARAGRAPHS.map((paragraph) => (
-                  <p key={paragraph}>{paragraph}</p>
-                ))}
+                <Typography variant='card-title-accent'>
+                  Additional Learning
+                </Typography>
+                <Typography variant='heading-2'>
+                  Structured learning outside formal education
+                </Typography>
               </div>
-            </Typography>
 
-            <div className='flex flex-col gap-4'>
-              {ADDITIONAL_LEARNING_ITEMS.map((item) => {
-                return (
-                  <Card key={item.title} variant='inner' padding='small'>
-                    <div className='flex flex-col gap-2'>
-                      <Typography variant='heading-4'>{item.title}</Typography>
-                      <Typography variant='body'>{item.text}</Typography>
-                    </div>
-                  </Card>
-                );
-              })}
+              <Typography variant='body'>
+                <div className='flex flex-col gap-2'>
+                  {ADDITIONAL_LEARNING_PARAGRAPHS.map((paragraph) => (
+                    <p key={paragraph}>{paragraph}</p>
+                  ))}
+                </div>
+              </Typography>
+
+              <div className='flex flex-col gap-4'>
+                {ADDITIONAL_LEARNING_ITEMS.map((item) => {
+                  return (
+                    <Card key={item.title} variant='inner' padding='small'>
+                      <div className='flex flex-col gap-2'>
+                        <Typography variant='heading-4'>
+                          {item.title}
+                        </Typography>
+                        <Typography variant='body'>{item.text}</Typography>
+                      </div>
+                    </Card>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        </Card>
-
-        {TAG_CARD_TAGS.map((tags) => getTagCard(tags))}
+          </Card>
+        </div>
       </div>
     </PageContainer>
   );
 }
 
-function getTagCard(tags: readonly string[]): ReactNode {
-  return (
-    <Card className='lg:col-span-2' padding='medium'>
-      <div className='flex flex-col gap-3'>
-        <Typography variant='card-title-muted'>Driver's license</Typography>
-        <div className='flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <Tag key={tag} label={tag} />
-          ))}
-        </div>
-      </div>
-    </Card>
-  );
-}
-
-const EDUCATION_HIGHLIGHTS: readonly string[] = [
-  'Five year course, total 284 ECTS credits.',
-  'Gives good foundation in mathematics, physics, and electrical engineering in the first two years.',
-  'Later part is focused on programming, computer science, and software engineering.',
-  'Graduation thesis was in the domain of computer graphics - a BVH animation viewer that parses motion capture files and renders the animation.',
+const PROJECT_PERSONAL_WEBSITE_TAGS: readonly string[] = [
+  'React',
+  'TypeScript',
+  'Tailwind CSS',
+  'Vite',
+  'Bun',
+  'ESLint',
+  'Prettier',
+  'Storybook',
 ];
 
-const THESIS_LINKS: readonly LinkItem[] = [
+const PROJECT_PERSONAL_WEBSITE_LINKS: readonly LinkItem[] = [
   {
-    href: 'https://www.fer.unizg.hr/en',
-    label: 'Faculty of Electrical Engineering and Computing (FER)',
+    href: 'https://mrzli.github.io/',
+    label: 'Site',
   },
   {
-    href: 'http://www.zemris.fer.hr/predmeti/rg/diplomski/08Mrzljak/bvhviewer_pages/html/index.html',
-    label: 'Short summary of the thesis on the faculty site',
-  },
-  {
-    href: 'http://www.zemris.fer.hr/predmeti/rg/diplomski/08Mrzljak/bvhviewer_pages/html/files/Diplomski.doc',
-    label: 'Full thesis (.doc file)',
-  },
-  {
-    href: 'https://github.com/mrzli/bvhviewer',
-    label: 'Source code for the thesis application',
-  },
-  {
-    href: 'https://youtu.be/7u4C-OBQZIk',
-    label: 'Short video showing the thesis application',
+    href: 'https://github.com/mrzli/mrzli.github.io',
+    label: 'Source Code',
   },
 ];
 
@@ -165,16 +146,4 @@ const ADDITIONAL_LEARNING_ITEMS: readonly AdditionalLearningItem[] = [
     title: 'Other Topics',
     text: 'Compilers (simple ones), code generation, foundational machine learning course on Coursera, some algorithms and data structures courses, Data Visualization (D3), some economics and statistics classes.',
   },
-];
-
-const LOCATIONS: readonly string[] = ['Zagreb, Croatia'];
-
-const LANGUAGES: readonly string[] = ['Croatian - native', 'English - fluent'];
-
-const DRIVER_LICENSE_CATEGORIES: readonly string[] = ['AM', 'B', 'F', 'G'];
-
-const TAG_CARD_TAGS: readonly (readonly string[])[] = [
-  LOCATIONS,
-  LANGUAGES,
-  DRIVER_LICENSE_CATEGORIES,
 ];
