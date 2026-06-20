d6ffdfc8096ced8e2c94fb1c5130496dc60aa35a
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-20 19:36:16 +0200

    update test page

--- src/app/pages/test/components/project-card.tsx
+++ src/app/pages/test/components/project-card.tsx
@@ -5,9 +5,9 @@ import type { LinkItem } from '../../../types';
 
 export interface ProjectCardProps {
   readonly title: string;
-  readonly text: string;
+  readonly text: readonly string[];
   readonly tags: readonly string[];
-  readonly roleText: string;
+  readonly roleText: readonly string[];
   readonly links: readonly LinkItem[];
 }
 
@@ -22,13 +22,25 @@ export function ProjectCard({
     <Card padding='large'>
       <div className='flex flex-col gap-4'>
         <Typography variant='card-title-accent'>{title}</Typography>
-        <Typography variant='body'>{text}</Typography>
+        <Typography variant='body'>
+          <div className='flex flex-col gap-2'>
+            {text.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </div>
+        </Typography>
         <TagList tags={tags} />
 
         <Card variant='inner' padding='small'>
           <div className='flex flex-col gap-2'>
             <Typography variant='heading-4'>My role</Typography>
-            <Typography variant='body'>{roleText}</Typography>
+            <Typography variant='body'>
+              <div className='flex flex-col gap-2'>
+                {roleText.map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+              </div>
+            </Typography>
           </div>
         </Card>
 
--- src/app/pages/test/data.ts
+++ src/app/pages/test/data.ts
@@ -3,7 +3,9 @@ import type { ProjectCardProps } from './components';
 export const PROJECT_ITEMS: readonly ProjectCardProps[] = [
   {
     title: 'Personal Website',
-    text: 'You are currently viewing it. It is just a simple static website displaying information relevant to my professional life.',
+    text: [
+      'You are currently viewing it. It is just a simple static website displaying information relevant to my professional life.',
+    ],
     tags: [
       'React',
       'TypeScript',
@@ -14,16 +16,61 @@ export const PROJECT_ITEMS: readonly ProjectCardProps[] = [
       'Prettier',
       'Storybook',
     ],
-    roleText:
+    roleText: [
       'I did all the development. The site is static and very simple. All of the content of the pages is simply hardcoded into the components.',
+    ],
     links: [
       {
-        href: 'https://mrzli.github.io/',
         label: 'Site',
+        href: 'https://mrzli.github.io/',
       },
       {
-        href: 'https://github.com/mrzli/mrzli.github.io',
         label: 'Source Code',
+        href: 'https://github.com/mrzli/mrzli.github.io',
+      },
+    ],
+  },
+  {
+    title: 'Symbol Jump',
+    text: [
+      'A clone of a popular game called Doodle Jump. Complete and fully featured.',
+    ],
+    tags: [
+      'Java',
+      'libGDX',
+      'Android SDK',
+      'Facebook SDK for Android',
+      'Sinatra (Ruby)',
+      'Apache',
+      'MySQL',
+    ],
+    roleText: [
+      'This was a personal project so I wrote all the code and created all the graphics. The graphics are black and white and pretty simple.',
+      'I also implemented a global score service on a VPS server that was accessed from the game by asynchronous http calls. The server is no longer active.',
+    ],
+    links: [{ label: 'Source', href: 'https://github.com/mrzli-other/tria' }],
+  },
+  {
+    title: 'Hell Hopper',
+    text: ['A more primitive predecessor of the Symbol Jump game.'],
+    tags: ['Java', 'libGDX', 'Android SDK'],
+    roleText: ['I wrote all the code and created all the graphics.'],
+    links: [
+      {
+        label: 'Source',
+        href: 'https://github.com/mrzli-other/hellhopper',
+      },
+    ],
+  },
+  {
+    title: 'Suchy Blocks',
+    text: ['Tetris clone, available for Android and desktop.'],
+    tags: ['Java', 'libGDX', 'Android SDK'],
+    roleText: ['I wrote all the code and created all the graphics.'],
+    links: [
+      {
+        label: 'Source',
+        href: 'https://github.com/mrzli-other/suchyblocks',
       },
     ],
   },
