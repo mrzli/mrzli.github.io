e440b3cb6bf9c45a8aa8e791191ed7f35b23e258
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-22 22:05:39 +0200

    update exprience data

--- src/app/pages/test/data.ts
+++ src/app/pages/test/data.ts
@@ -2,17 +2,90 @@ import type { ExperienceSectionProps } from './components';
 
 export const EXPERIENCE_SECTIONS: readonly ExperienceSectionProps[] = [
   {
-    title: '',
-    location: '',
-    role: '',
-    from: { year: 2007, month: 1 },
-    to: { year: 2008, month: 12 },
+    title: 'Freelance software developer / Self Employed',
+    location: 'Remote',
+    role: 'Full Stack Web Developer (Primarily)',
+    from: { year: 2016, month: 1 },
+    to: 'Present',
     projects: [
       {
         title: '',
         text: [],
         tags: [],
         roleText: [],
+      },
+      {
+        title: 'Other Projects',
+        text: [
+          'Simple service used for exporting data from database in JSON format, built using C# and WCF. The target database is Microsoft SQL Server.',
+          'Some emergency work on an Alfresco based web app for a client very close to production deadline.',
+          'Work on backend for a talent acquisition software built using Ruby.',
+          'Small project used for talent management (HR/recruitment) built with React.',
+        ],
+        tags: ['C#', 'WCF', 'Ruby', 'React', 'Java', 'Alfresco'],
+        roleText: [
+          'I did the first service mentioned completely by myself.',
+          'The Alfresco work was just some bug fixing for a couple of weeks. It would not usually be worth mentioning, but it is an interesting example of a project I was brought onto literally a week or two before production, to a code base and the platform (Alfresco) I was unfamiliar with. I did help them, and the client was satisfied with my help, but it is not the kind of work I usually do. The help was not groundbreaking, but I was able to help with their need for additional manpower at a very late hour and on very short notice. This provided an intro to additional work for the same client.',
+          'The last two projects were small projects, each lasting several weeks. The work on these projects was split evenly between me and another programmer.',
+        ],
+      },
+    ],
+  },
+  {
+    title: 'APIS IT',
+    location: 'Zagreb (Croatia)',
+    role: 'C# Developer',
+    from: { year: 2013, month: 11 },
+    to: { year: 2016, month: 5 },
+    projects: [
+      {
+        title: "Croatia's Ministry of Finance e-Tax Web Application",
+        text: [
+          'This it a large scale product, with 1M+ daily views.',
+          'It is the official product that allows citizens and businesses in Croatia to submit their tax forms electronically. It supports many different tax forms to handle all the relevant types of tax reporting. The application has many other features, but tax reporting (form submission) is probably the main use case.',
+        ],
+        tags: [
+          'C#',
+          'ASP.NET Web Forms',
+          'jQuery',
+          'Microsoft Reports (.rdlc)',
+          'IBM DB2',
+        ],
+        roleText: [
+          'I fully implemented 7 of the tax forms used in production as of March 2016. Additionally, I did varying amounts of work on about 10 other forms, ranging from bug fixes, to implementing large parts of entire forms. At that time, there were a total of about 25 tax forms in the entire system.',
+          'Implementing a tax form including doing a web form, XML import and export, PDF reporting, extensive server-side validation based on business and legal rules, and tax form specific changes to the data access layer.',
+        ],
+      },
+      {
+        title: 'Location and Building Permits Web Application',
+        text: [
+          "This is a relatively simple application used for submission of data relevant to different types of location and building permits. It was built for Croatia's Ministry of Construction and Physical Planning, to be used by citizens (general population).",
+        ],
+        tags: [
+          'C#',
+          'ASP.NET MVC',
+          'jQuery',
+          'Fluent Validation',
+          'Aspose.Words',
+          'Oracle Database',
+        ],
+        roleText: [
+          'I implemented large chunks of the project. Much of the UI part (views, models, controllers), significant part of the underlying business logic, the entire data access layer and PDF reporting.',
+        ],
+      },
+      {
+        title: 'Other Projects',
+        text: [
+          'Ny time on this job was spent on many other projects as well. This included full stack development on two other very large ASP.NET Web Forms applications to be used by the employees of Ministry of Construction and Physical Planning. These are different applications from the one mentioned above.',
+          'Several WCF and ASMX web services as part of the above projects or as standalone components in a larger system.',
+          'Software components used for XML processing: validation, data manipulation, data to XML export, XML to database import.',
+          'Various programs for offline data processing and validation, email error reporting etc.',
+        ],
+        tags: ['C#', 'ASP.NET Web Forms', '.NET COM', 'WCF'],
+        roleText: [
+          'Regarding the two large ASP.NET Web forms application. These applications were mostly in maintanance phase. While I did some larger chunks of work there, most of my work was just bug fixes and small feature development.',
+          'A lot of the rest were mini projects by themselves (although fitting into largers systems). They usually lasted a few days to few weeks. I mostly implemented those completely by myself.',
+        ],
       },
     ],
   },
@@ -71,7 +144,7 @@ export const EXPERIENCE_SECTIONS: readonly ExperienceSectionProps[] = [
         ],
       },
       {
-        title: 'Various Projects',
+        title: 'Other Projects',
         text: [
           'There are other projects I worked on that are used in the inspection process.',
         ],
