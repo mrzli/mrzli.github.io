d8a70ce8d088f3743076fac5c00809e866c85001
Goran Mržljak <goran.mrzljak@gmail.com>
2026-06-25 18:17:03 +0200

    fix routing isssue on gh pages

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -10,43 +10,48 @@ import { ProjectsPage } from '../app/pages/projects/projects-page';
 import { SkillsPage } from '../app/pages/skills/skills-page';
 import { TestPage } from '../app/pages/test/test-page';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: 'experience',
+          element: <ExperiencePage />,
+        },
+        {
+          path: 'skills',
+          element: <SkillsPage />,
+        },
+        {
+          path: 'projects',
+          element: <ProjectsPage />,
+        },
+        {
+          path: 'background',
+          element: <BackgroundPage />,
+        },
+        {
+          path: 'values',
+          element: <ValuesPage />,
+        },
+        {
+          path: 'test',
+          element: <TestPage />,
+        },
+        {
+          path: '*',
+          element: <Navigate to='/' replace />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'experience',
-        element: <ExperiencePage />,
-      },
-      {
-        path: 'skills',
-        element: <SkillsPage />,
-      },
-      {
-        path: 'projects',
-        element: <ProjectsPage />,
-      },
-      {
-        path: 'background',
-        element: <BackgroundPage />,
-      },
-      {
-        path: 'values',
-        element: <ValuesPage />,
-      },
-      {
-        path: 'test',
-        element: <TestPage />,
-      },
-      {
-        path: '*',
-        element: <Navigate to='/' replace />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
