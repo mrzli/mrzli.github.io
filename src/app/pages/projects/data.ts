adac33453421cc3bd9148b9e901a014afa3c0abe
Goran Mržljak <goran.mrzljak@gmail.com>
2026-09-14 20:37:43 +0200

    update projects texts

--- src/app/pages/projects/data.ts
+++ src/app/pages/projects/data.ts
@@ -3,12 +3,10 @@ import type { ProjectCardProps } from './components';
 export const PROJECT_ITEMS: readonly ProjectCardProps[] = [
   {
     title: 'Personal Website',
-    text: [
-      'You are currently viewing it. It is just a simple static website displaying information relevant to my professional life.',
-    ],
+    text: ['This website presents my professional experience, skills, projects, and background.'],
     tags: ['React', 'TypeScript', 'Tailwind CSS', 'Vite', 'Bun', 'ESLint', 'Prettier', 'Storybook'],
     roleText: [
-      'I did all the development. The site is static and very simple. All of the content of the pages is simply hardcoded into the components.',
+      'I built the entire site. It is static, with content stored in TypeScript data files and React components.',
     ],
     links: [
       {
@@ -23,7 +21,7 @@ export const PROJECT_ITEMS: readonly ProjectCardProps[] = [
   },
   {
     title: 'Symbol Jump',
-    text: ['A clone of a popular game called Doodle Jump. Complete and fully featured.'],
+    text: ['A complete, fully featured clone of Doodle Jump.'],
     tags: [
       'Java',
       'libGDX',
@@ -34,31 +32,31 @@ export const PROJECT_ITEMS: readonly ProjectCardProps[] = [
       'MySQL',
     ],
     roleText: [
-      'This was a personal project so I wrote all the code and created all the graphics. The graphics are black and white and pretty simple.',
-      'I also implemented a global score service on a VPS server that was accessed from the game by asynchronous http calls. The server is no longer active.',
+      'I wrote all the code and created the simple black-and-white graphics.',
+      'I also built a global score service on a VPS, accessed by the game through asynchronous HTTP calls. The server is no longer active.',
     ],
-    links: [{ label: 'Source', href: 'https://github.com/mrzli-other/tria' }],
+    links: [{ label: 'Source Code', href: 'https://github.com/mrzli-other/tria' }],
   },
   {
     title: 'Hell Hopper',
-    text: ['A more primitive predecessor of the Symbol Jump game.'],
+    text: ['A simpler predecessor to Symbol Jump.'],
     tags: ['Java', 'libGDX', 'Android SDK'],
     roleText: ['I wrote all the code and created all the graphics.'],
     links: [
       {
-        label: 'Source',
+        label: 'Source Code',
         href: 'https://github.com/mrzli-other/hellhopper',
       },
     ],
   },
   {
     title: 'Suchy Blocks',
-    text: ['Tetris clone, available for Android and desktop.'],
+    text: ['A Tetris clone for Android and desktop.'],
     tags: ['Java', 'libGDX', 'Android SDK'],
     roleText: ['I wrote all the code and created all the graphics.'],
     links: [
       {
-        label: 'Source',
+        label: 'Source Code',
         href: 'https://github.com/mrzli-other/suchyblocks',
       },
     ],
